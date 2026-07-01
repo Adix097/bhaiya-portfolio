923e53598cbb474fe05b374852d9ec8d3583a061
adix097 <adityav181216@gmail.com>
2026-07-01 22:07:02 +0530

    changed medium to category

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -37,8 +37,8 @@ const Collections = () => {
   return (
     <div className="columns-2 gap-3 md:columns-3 xl:columns-4">
       {collections.map((collection) => {
-        const medium = collection.metadata?.find(
-          (m) => m.label === "Medium",
+        const category = collection.metadata?.find(
+          (m) => m.label === "Category",
         )?.value;
 
         return (
@@ -49,13 +49,13 @@ const Collections = () => {
           >
             <img
               src={collection.coverImage.url}
-              alt={medium}
+              alt={category}
               loading="lazy"
               className="w-full object-cover"
             />
             <div className="absolute inset-0 flex flex-col items-center text-center justify-center bg-black/60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               <h3 className="text-card-heading p-5 font-thin text-white">
-                {medium}
+                {category}
               </h3>
             </div>
           </Link>
