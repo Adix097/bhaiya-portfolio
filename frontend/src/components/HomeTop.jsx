c08833c62af52ebec15f21fda6fd816acafa800e
adix097 <adityav181216@gmail.com>
2026-07-14 21:42:40 +0530

    refactor: new favicon

--- frontend/src/components/HomeTop.jsx
+++ frontend/src/components/HomeTop.jsx
@@ -1,25 +1,19 @@
-import heroImg from "../assets/hero-img.png";
+import phoneImg from "../assets/phone-img.png";
 
 const HomeTop = () => {
   return (
-    <div className="relative w-full overflow-hidden rounded-4xl bg-(--surface-raised) shadow-[0_25px_80px_rgba(0,0,0,0.18)]">
+    <div className="relative h-[55vh] overflow-hidden">
       <img
-        src={heroImg}
-        alt="Saurav Vishwakarma"
-        className="h-[55vh] w-full object-cover object-center sm:h-[65vh] lg:h-[78vh] animate-[heroZoom_8s_ease-out_forwards]"
+        src={phoneImg}
+        alt=""
+        className="absolute inset-0 h-[115%] w-full object-contain object-top"
+        style={{
+          WebkitMaskImage:
+            "linear-gradient(to bottom, black 0%, black 75%, transparent 100%)",
+          maskImage:
+            "linear-gradient(to bottom, black 0%, black 75%, transparent 100%)",
+        }}
       />
-
-      {/* Top */}
-      <div className="pointer-events-none absolute inset-x-0 top-0 h-20 bg-linear-to-b from-(--background) to-transparent" />
-
-      {/* Bottom */}
-      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-20 bg-linear-to-t from-(--background) to-transparent" />
-
-      {/* Left */}
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-20 bg-linear-to-r from-(--background) to-transparent" />
-
-      {/* Right */}
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-20 bg-linear-to-l from-(--background) to-transparent" />
     </div>
   );
 };
