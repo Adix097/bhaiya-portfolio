3ea122d20c23daac6ff7799a7965498cdf7772ec
Aditya <122256697+Adix097@users.noreply.github.com>
2026-07-25 16:43:05 +0000

    Update contact.js

--- backend/src/routes/contact.js
+++ backend/src/routes/contact.js
@@ -24,7 +24,9 @@ const escapeHtml = (text) => {
 };
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
