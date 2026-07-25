1f37e99bb012a4710b25bc789e5481d3b5bcb04d
Aditya <122256697+Adix097@users.noreply.github.com>
2026-07-25 16:42:04 +0000

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT ?? 5000;
 // Middleware
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
+app.set('trust proxy', 1);
 
 // Routes
 app.use("/api/collections", collectionsRouter);
